82d31ecccc69965326763891073fb529160e8deb
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-03-30 17:04:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // your code here
+const form = document.querySelector("form");
+const h3 = document.querySelector("#url");
+form.addEventListener("submit",()=>{
+	h3.innerText="appropriate query string";
+})
